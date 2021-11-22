ebc4d10f33e5cb52df6923f9c5c70078beaf69c2
manolofc <manolofc@gmail.com>
2021-11-22 18:21:48 +0100

    WIP: sorting out new ros2 - foxy branch 	- created msg for Handle forces 	- created usb publisher node with launcher (TODO: parametrise  published data int/double) TODO: upload Arduino codes for handles and wheels!

---
+++ walker2_driver/src/USBConn.cpp
@@ -0,0 +1,186 @@
+
+#include "walker2_driver/USBConn.hpp"
+
+HandlePublisher::HandlePublisher() : Node("handle_publisher"){
+        /*
+        TODO: I got some problems using arguments and params with ':' or '\n' symbols,
+        so I'm hardcoding them here.
+        */
+        
+        this->declare_parameter<std::string>( "serial_preamble",  "RL" ); 
+        this->get_parameter("serial_preamble", preamble_);
+        preamble_ = preamble_ + ":";
+        endMarker_ = '\n';
+ 
+        this->declare_parameter<std::string>( "handle_topic_name", "left_handle" ); 
+        this->get_parameter("handle_topic_name", topic_name_);
+
+        this->declare_parameter<std::string>( "handle_frame_id",  "left_handle_id" ); 
+        this->get_parameter("handle_frame_id", frame_id_);
+
+        this->declare_parameter<std::string>( "port_basename",  "/dev/ttyUSB") ; 
+        this->get_parameter("port_basename", basename_ );
+        
+        publisher_ = this->create_publisher<walker2_msgs::msg::W2Hand>(topic_name_, 10);
+
+        port_name_ = this->get_serial_port(preamble_, 8); 
+        
+        try{
+            // Open the Serial Port at the desired hardware port.
+            serial_stream_.Open(port_name_) ;
+        }    catch (const OpenFailed&) {
+            RCLCPP_FATAL(this->get_logger(), "The serial port [%s] did not open correctly", port_name_.c_str() ); 
+        }
+
+        // Set the baud rate of the serial port.
+        serial_stream_.SetBaudRate(BaudRate::BAUD_115200) ;
+        // Set the number of data bits.
+        serial_stream_.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
+        // Turn off hardware flow control.
+        serial_stream_.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
+        // Disable parity.
+        serial_stream_.SetParity(Parity::PARITY_NONE) ;
+        // Set the number of stop bits.
+        serial_stream_.SetStopBits(StopBits::STOP_BITS_1) ;
+
+}
+
+void HandlePublisher::start(){
+        bool isRecvStarted = false;
+        char rc;
+        std::stringstream ss;
+        std::string data;
+        walker2_msgs::msg::W2Hand measurement;
+
+        measurement.header.frame_id = frame_id_;
+        RCLCPP_INFO(this->get_logger(), "Publishing handle measurements at [%s]", topic_name_.c_str());
+
+        while (rclcpp::ok()) {
+            serial_stream_.get( rc );
+
+            if (isRecvStarted == true) {
+                if (rc != endMarker_) {
+                    ss << rc;
+                } else {
+                    data = ss.str();
+                    measurement.header.stamp = this->get_clock()->now();
+                    // remove preamble and cast data to number:
+                    measurement.force = std::stof(data.substr(preamble_.length()));
+                    RCLCPP_DEBUG(this->get_logger(), "data is: [%3.2f]", measurement.force);
+
+                    // send data
+                    try {
+                        publisher_->publish(measurement);
+                    } catch (const rclcpp::exceptions::RCLError &e) {
+                        RCLCPP_ERROR( this->get_logger(), "unexpectedly failed with %s", e.what());
+                    }
+
+                    // clear stream and carry on
+                    ss.str(std::string());
+
+                }
+            }
+
+            else if (rc == endMarker_) {
+                isRecvStarted = true;
+            }
+
+            try {
+                rclcpp::spin_some(this->get_node_base_interface());
+            } catch (const rclcpp::exceptions::RCLError &e) {
+                RCLCPP_ERROR( this->get_logger(), "unexpectedly failed with %s", e.what());
+            }
+
+        }
+        serial_stream_.Close();
+
+}
+
+std::string HandlePublisher::get_serial_port(std::string preamble, int highest_port) {
+        std::string port_name;
+        std::stringstream name_builder;
+        SerialStream my_serial_stream;
+        bool isSearchStarted = false;
+        std::stringstream sb;
+        std::string data;
+        char rc;
+
+        int attempts = 0;
+        int max_attempts = 5;
+
+        for (int i=0;i<=highest_port;i++){
+            // build a port name
+            // this adds padding to the number
+            //name_builder << basename << std::setw(2) << std::setfill('0') << i;
+            name_builder << basename_  << i;
+            port_name = name_builder.str();
+            name_builder.str(std::string());
+
+            // open it
+            try {
+                // Open the Serial Port at the desired hardware port.
+                RCLCPP_DEBUG(this->get_logger(), "Opening serial port [%s].", port_name.c_str());
+                my_serial_stream.Open( port_name );
+            } catch (const OpenFailed&) {
+                RCLCPP_ERROR(this->get_logger(), "The serial port [%s] couldn't be opened.", port_name.c_str());
+                my_serial_stream.Close(  );
+                continue;
+            }
+            RCLCPP_DEBUG(this->get_logger(), "Serial port [%s] opened", port_name.c_str());
+            
+            // Set the baud rate of the serial port.
+            my_serial_stream.SetBaudRate(BaudRate::BAUD_115200) ;
+            // Set the number of data bits.
+            my_serial_stream.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
+            // Turn off hardware flow control.
+            my_serial_stream.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
+            // Disable parity.
+            my_serial_stream.SetParity(Parity::PARITY_NONE) ;
+            // Set the number of stop bits.
+            my_serial_stream.SetStopBits(StopBits::STOP_BITS_1) ;
+
+
+            // Wait for data to be available at the serial port.
+            while( (my_serial_stream.rdbuf()->in_avail() == 0) && (attempts<max_attempts) ){
+                usleep(1000);
+                RCLCPP_DEBUG(this->get_logger(), "Waiting for data at [%s].", port_name.c_str());
+                attempts ++;
+            }
+
+            if (attempts<max_attempts) {
+                RCLCPP_DEBUG(this->get_logger(), "The serial port [%s] didn't show data after a while... skipping.", port_name.c_str());
+                my_serial_stream.Close();
+                continue;
+            }
+
+            // check if streamed data at port starts with the right preamble
+            isSearchStarted = false;
+            sb.str(std::string());
+            RCLCPP_DEBUG(this->get_logger(), "Looking for preamble [%s] ", preamble.c_str());
+            while (true) {
+                my_serial_stream.get( rc );
+                // search for preamble starts AFTER endMarker
+                if (isSearchStarted == true) {
+                    sb << rc;
+                    data = sb.str();
+                    if (data.compare(preamble_)==0) {
+                        //port found!
+                        RCLCPP_DEBUG(this->get_logger(), "Port found!. [%s] is using preamble [%s]", port_name.c_str(), preamble.c_str());
+                        my_serial_stream.Close();
+                        return port_name;
+                    }
+                } else if (rc == endMarker_) {
+                    if (!isSearchStarted){ 
+                        isSearchStarted = true;
+                    } else{
+                        RCLCPP_DEBUG(this->get_logger(), "The serial port [%s] didn't have preamble [%s] but [%s]", port_name.c_str(), preamble_.c_str(), data.c_str());
+                        my_serial_stream.Close();
+                        continue;
+                    }
+                }
+            }   
+        }
+        RCLCPP_ERROR(this->get_logger(), "Couldn't find preamble [%s] in ports [%s0] to [%s%i]", preamble_.c_str(),basename_.c_str(),basename_.c_str(),highest_port);                
+        return "";
+}
+
