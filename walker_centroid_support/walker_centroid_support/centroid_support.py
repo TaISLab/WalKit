0b8fcd16bf9ce5f07ad24344b184fbd6b2d9f63f
Manuel Fernandez-Carmona <manolofc@gmail.com>
2022-05-19 13:50:25 +0200

    testing centroid module

--- walker_centroid_support/walker_centroid_support/centroid_support.py
+++ walker_centroid_support/walker_centroid_support/centroid_support.py
@@ -1,13 +1,18 @@
 import rclpy
 from rclpy.node import Node
-
-from walker_msgs.msg import ForceStamped, StepStamped 
+from tf2_ros.buffer import Buffer
+from ament_index_python.packages import get_package_share_directory
+from tf2_ros.transform_listener import TransformListener
 from std_msgs.msg import Float64MultiArray, String
+from geometry_msgs.msg import PointStamped
+
 from scipy import interpolate
 import yaml
 from ament_index_python.packages import get_package_share_directory
 import os
+import numpy as np
 
+from walker_msgs.msg import ForceStamped, StepStamped 
 
 class CentroidSupport(Node):
 
@@ -16,19 +21,15 @@ class CentroidSupport(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                # ('handle_calibration_file', os.path.join(get_package_share_directory('walker_loads'), "config", "params.yaml") ),
+                 ('handle_calibration_file', os.path.join(get_package_share_directory('walker_loads'), "config", "params.yaml") ),
                  ('left_loads_topic_name', '/left_loads'),
                  ('right_loads_topic_name', '/right_loads'),
                  ('left_handle_topic_name', '/left_handle'),
                  ('right_handle_topic_name', '/right_handle'),
-                # ('left_steps_topic_name', '/detected_step_left'),
-                # ('right_steps_topic_name', '/detected_step_right'),
-                # ('user_desc_topic_name', '/user_desc'),
-                # ('period', 0.1),
-                # ('speed_delta', 0.05),
-                # ('force_calibration.weight_points', None),
-                # ('force_calibration.left_handle_points', None),
-                # ('force_calibration.right_handle_points', None)
+                 ('user_desc_topic_name', '/user_desc'),
+                 ('period', 0.05),
+                 ('speed_delta', 0.05),
+                 ('centroid_topic_name', '/support_centroid'),
             ]
         )
 
@@ -36,10 +37,9 @@ class CentroidSupport(Node):
         self.left_loads_topic_name = self.get_parameter('left_loads_topic_name').value
         self.right_loads_topic_name = self.get_parameter('right_loads_topic_name').value
         self.left_handle_topic_name = self.get_parameter('left_handle_topic_name').value
-        self.right_handle_topic_name= self.get_parameter('right_handle_topic_name').value
-        self.left_steps_topic_name= self.get_parameter('left_steps_topic_name').value
-        self.right_steps_topic_name= self.get_parameter('right_steps_topic_name').value
-        self.user_desc_topic_name= self.get_parameter('user_desc_topic_name').value
+        self.right_handle_topic_name = self.get_parameter('right_handle_topic_name').value
+        self.user_desc_topic_name = self.get_parameter('user_desc_topic_name').value
+        self.centroid_topic_name = self.get_parameter('centroid_topic_name').value
         self.period = self.get_parameter('period').value
         self.speed_delta = self.get_parameter('speed_delta').value
         self.handle_calibration_file = self.get_parameter('handle_calibration_file').value
@@ -62,24 +62,30 @@ class CentroidSupport(Node):
         self.left_step_msg = None
         self.right_step_msg = None
 
-        self.speed_diff = 0
-        self.right_speed = None
-        self.left_speed  = None
-
         self.weight = 100
         self.right_handle_weight = 0
         self.left_handle_weight  = 0
         self.leg_load = 0
 
+        self.right_handle_pose = None
+        self.left_handle_pose = None
+
+        # ROS stuff        
+        self.buffer = Buffer()
+        self.listener = TransformListener(self.buffer, self)
+
+        self.new_data_available = False
+        self.first_data_ready = False
+        
         # ROS stuff
-        self.left_load_pub  = self.create_publisher(StepStamped, self.left_loads_topic_name,  10)
-        self.right_load_pub = self.create_publisher(StepStamped, self.right_loads_topic_name, 10)
+        self.centroid_pub = self.create_publisher(PointStamped, self.centroid_topic_name, 10)
 
         self.left_handle_sub = self.create_subscription(ForceStamped, self.left_handle_topic_name, self.handle_lc, 10) 
         self.right_handle_sub = self.create_subscription(ForceStamped, self.right_handle_topic_name, self.handle_lc, 10) 
 
-        self.left_steps_sub = self.create_subscription(StepStamped, self.left_steps_topic_name,  self.l_steps_lc, 10) 
-        self.right_steps_sub = self.create_subscription(StepStamped, self.right_steps_topic_name, self.r_steps_lc, 10)
+        self.left_load_sub  = self.create_subscription(StepStamped, self.left_loads_topic_name,  self.l_steps_lc, 10)
+        self.right_load_sub = self.create_subscription(StepStamped, self.right_loads_topic_name, self.r_steps_lc, 10)
+
 
         self.user_desc_sub = self.create_subscription(String, self.user_desc_topic_name, self.user_desc_lc, 10) 
         self.tmr = self.create_timer(self.period, self.timer_callback)
@@ -90,7 +96,29 @@ class CentroidSupport(Node):
     def user_desc_lc(self, msg):
         user_fields = msg.data.split(':')
         if len(user_fields)>2:
-            self.weight = int(user_fields[2])
+            new_weight = int(user_fields[2])
+            if (new_weight != self.weight):
+                self.weight = new_weight
+                self.new_data_available = True
+
+    def handle_lc(self, msg):
+        if ('right' in msg.header.frame_id ):
+            self.right_handle_msg = msg          
+            self.right_handle_weight = self.fr(msg.force)
+            self.new_data_available = True
+            # Get handle position in the same frame as the steps.
+            # We need this only once... unless you change steps frame over time?
+            if (self.right_handle_pose==None):
+                self.right_handle_pose= self.get_handle_pose(self.right_step_msg, self.right_handle_msg)
+        elif ('left' in msg.header.frame_id):
+            self.left_handle_msg = msg
+            self.left_handle_weight  = self.fl(msg.force) 
+            self.new_data_available = True
+            if (self.left_handle_pose==None):
+                self.left_handle_pose= self.get_handle_pose(self.left_step_msg, self.left_handle_msg)
+        else:
+            self.get_logger().error("Don't know about which handle are you talking [" + msg.header.frame_id + "]")    
+            return
 
     def l_steps_lc(self, msg):
         self.steps_lc(msg,0)
@@ -98,65 +126,63 @@ class CentroidSupport(Node):
     def r_steps_lc(self, msg):
         self.steps_lc(msg,1)
 
-    def handle_lc(self, msg):
-        if ('right' in msg.header.frame_id ):
-            self.right_handle_msg = msg          
-            self.right_handle_weight = self.fr(msg.force)
-        elif ('left' in msg.header.frame_id):
-            self.left_handle_msg = msg
-            self.left_handle_weight  = self.fl(msg.force) 
-        else:
-            self.get_logger().error("Don't know about which handle are you talking [" + msg.header.frame_id + "]")    
-            return                
-        self.leg_load = self.weight - self.left_handle_weight - self.right_handle_weight
-
-
     def steps_lc(self, msg, id):
         if  (id==1):
                 self.right_step_msg = msg
                 self.right_speed = self.right_step_msg.speed
+                self.new_data_available = True
         elif (id==0):
                 self.left_step_msg = msg
                 self.left_speed = self.left_step_msg.speed
+                self.new_data_available = True
         else:
             self.get_logger().error("Don't know about which step are you talking [" + id + "]")    
             return          
         
-        if ((self.left_step_msg is not None) and  (self.right_step_msg is not None) ): 
-            speed_diff_x = self.left_speed.x - self.right_speed.x
-            speed_diff_y = self.left_speed.y - self.right_speed.y
-            speed_diff_z = self.left_speed.z - self.right_speed.z
-            #self.speed_diff = pow(speed_diff_x * speed_diff_x + speed_diff_y * speed_diff_y + speed_diff_z * speed_diff_z , 0.5)
-            self.speed_diff = speed_diff_x
-
     def timer_callback(self):
-        if ( (self.left_handle_msg is not None) and  (self.right_handle_msg is not None) and (self.left_step_msg is not None) and  (self.right_step_msg is not None) ):
-                if (self.speed_diff>self.speed_delta):
-                    self.right_leg_load = self.leg_load
-                    self.left_leg_load = 0.0
-                elif (self.speed_diff<-self.speed_delta):
-                    self.right_leg_load = 0.0
-                    self.left_leg_load = self.leg_load
-                else:
-                    # both leg standing. We can't be sure about how much on each one!
-                    self.right_leg_load = -1.0 #0.5 * self.leg_load
-                    self.left_leg_load =  -1.0 #0.5 * self.leg_load
-                   
-                # Build msg and publish
-                # left
-                msg = self.left_step_msg                
-                msg.load = self.left_leg_load
-                self.left_load_pub.publish(msg)
+        if (self.new_data_available):
+            self.new_data_available = False
 
-                #right
-                msg = self.right_step_msg                
-                msg.load = self.right_leg_load
-                self.right_load_pub.publish(msg)
-                #self.get_logger().warn("Weight distribution on legs L(" + str(self.left_leg_load) + ") - R(" + str(self.right_leg_load) + ")")
+            if (not self.first_data_ready ):
+                self.first_data_ready = (self.left_handle_msg is not None) and  (self.right_handle_msg is not None) and (self.left_step_msg is not None) and  (self.right_step_msg is not None)    
+
+            if ( self.first_data_ready ):
+                    right_step_position = np.array([self.right_step_msg.position.point.x, self.right_step_msg.position.point.y, 0])
+                    left_step_position = np.array([self.left_step_msg.position.point.x, self.left_step_msg.position.point.y, 0])
+                    right_handle_position = np.array([self.right_handle_pose.position.point.x, self.right_handle_pose.position.point.y, self.right_handle_pose.position.point.z])
+                    left_handle_position = np.array([self.left_handle_pose.position.point.x, self.left_handle_pose.position.point.y, self.left_handle_pose.position.point.z])
+
+                    centroid = ( self.right_leg_load      * right_step_position   + 
+                                 self.left_leg_load       * left_step_position    + 
+                                 self.right_handle_weight * right_handle_position + 
+                                 self.left_handle_weight  * left_handle_position  ) / self.weight
+
+                    centroid_msg = PointStamped()
+                    centroid_msg.header = self.right_step_msg.header
+                    centroid_msg.position.point.x = centroid[0]
+                    centroid_msg.position.point.y = centroid[1]
+                    centroid_msg.position.point.z = centroid[2]
+                    
+                    self.centroid_pub.publish(centroid_msg)
 
         else:
             pass
-            #self.get_logger().error("Not all data received yet ...")                
+                #self.get_logger().error("Not all data received yet ...")
+
+    # We take 0,0,0 in handles frame and cast it to steps frame.
+    def get_handle_pose(self, step_msg, handle_msg):
+        handle_pos = None
+        if ( (step_msg is not None) and (handle_msg is not None)):
+            handle_orig_rel = PointStamped()
+            handle_orig_rel.header = handle_msg.header
+            handle_orig_rel.point.x = handle_orig_rel.point.y = handle_orig_rel.point.z = 0
+            
+            try:
+                handle_pos = self.buffer.transform(handle_orig_rel, step_msg.position.header.frame_id)
+            except Exception as e:
+                self.get_logger().error("Can't transform handle point into frame [" + step_msg.position.header.frame_id + "]: [" + str(e) + "]")    
+        
+        return handle_pos
 
 def main(args=None):
     rclpy.init(args=args)
--- walker_loads/walker_loads/partial_loads.py
+++ walker_loads/walker_loads/partial_loads.py
@@ -6,8 +6,11 @@ from tf2_ros.transform_listener import TransformListener
 from std_msgs.msg import Float64MultiArray, String
 from geometry_msgs.msg import PointStamped
 
+from walker_msgs.msg import ForceStamped, StepStamped 
+from std_msgs.msg import Float64MultiArray, String
 from scipy import interpolate
 import yaml
+from ament_index_python.packages import get_package_share_directory
 import os
 import numpy as np
 
@@ -20,31 +23,27 @@ class PartialLoads(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('handle_calibration_file', os.path.join(get_package_share_directory('walker_loads'), "config", "params.yaml") ),
-                ('left_loads_topic_name', '/left_loads'),
-                ('right_loads_topic_name', '/right_loads'),
-                ('left_handle_topic_name', '/left_handle'),
-                ('right_handle_topic_name', '/right_handle'),
-                ('left_steps_topic_name', '/detected_step_left'),
-                ('right_steps_topic_name', '/detected_step_right'),
-                ('user_desc_topic_name', '/user_desc'),
-                ('centroid_topic_name', '/support_centroid'),
-                ('period', 0.1),
-                ('speed_delta', 0.05),
-                ('force_calibration.weight_points', None),
-                ('force_calibration.left_handle_points', None),
-                ('force_calibration.right_handle_points', None)
-            ])
+                 ('handle_calibration_file', os.path.join(get_package_share_directory('walker_loads'), "config", "params.yaml") ),
+                 ('left_loads_topic_name', '/left_loads'),
+                 ('right_loads_topic_name', '/right_loads'),
+                 ('left_handle_topic_name', '/left_handle'),
+                 ('right_handle_topic_name', '/right_handle'),
+                 ('left_steps_topic_name', '/detected_step_left'),
+                 ('right_steps_topic_name', '/detected_step_right'),
+                 ('user_desc_topic_name', '/user_desc'),
+                 ('period', 0.05),
+                 ('speed_delta', 0.05)
+            ]
+        )
 
 
         self.left_loads_topic_name = self.get_parameter('left_loads_topic_name').value
         self.right_loads_topic_name = self.get_parameter('right_loads_topic_name').value
         self.left_handle_topic_name = self.get_parameter('left_handle_topic_name').value
-        self.right_handle_topic_name = self.get_parameter('right_handle_topic_name').value
-        self.left_steps_topic_name = self.get_parameter('left_steps_topic_name').value
-        self.right_steps_topic_name = self.get_parameter('right_steps_topic_name').value
-        self.user_desc_topic_name = self.get_parameter('user_desc_topic_name').value
-        self.centroid_topic_name = self.get_parameter('centroid_topic_name').value
+        self.right_handle_topic_name= self.get_parameter('right_handle_topic_name').value
+        self.left_steps_topic_name= self.get_parameter('left_steps_topic_name').value
+        self.right_steps_topic_name= self.get_parameter('right_steps_topic_name').value
+        self.user_desc_topic_name= self.get_parameter('user_desc_topic_name').value
         self.period = self.get_parameter('period').value
         self.speed_delta = self.get_parameter('speed_delta').value
         self.handle_calibration_file = self.get_parameter('handle_calibration_file').value
@@ -76,19 +75,15 @@ class PartialLoads(Node):
         self.left_handle_weight  = 0
         self.leg_load = 0
 
-        self.right_handle_pose = None
-        self.left_handle_pose = None
-
-        # ROS stuff        
-        self.buffer = Buffer()
-        self.listener = TransformListener(self.buffer, self)
+        self.new_data_available = False
+        self.first_data_ready = False
         
+        # ROS stuff
         self.left_load_pub  = self.create_publisher(StepStamped, self.left_loads_topic_name,  10)
         self.right_load_pub = self.create_publisher(StepStamped, self.right_loads_topic_name, 10)
-        self.centroid_pub = self.create_publisher(PointStamped, self.centroid_topic_name, 10)
 
-        self.left_handle_sub = self.create_subscription(ForceStamped, self.left_handle_topic_name, self.l_handle_lc, 10) 
-        self.right_handle_sub = self.create_subscription(ForceStamped, self.right_handle_topic_name, self.r_handle_lc, 10) 
+        self.left_handle_sub = self.create_subscription(ForceStamped, self.left_handle_topic_name, self.handle_lc, 10) 
+        self.right_handle_sub = self.create_subscription(ForceStamped, self.right_handle_topic_name, self.handle_lc, 10) 
 
         self.left_steps_sub = self.create_subscription(StepStamped, self.left_steps_topic_name,  self.l_steps_lc, 10) 
         self.right_steps_sub = self.create_subscription(StepStamped, self.right_steps_topic_name, self.r_steps_lc, 10)
@@ -102,46 +97,39 @@ class PartialLoads(Node):
     def user_desc_lc(self, msg):
         user_fields = msg.data.split(':')
         if len(user_fields)>2:
-            self.weight = int(user_fields[2])
+            new_weight = int(user_fields[2])
+            if (new_weight != self.weight):
+                self.weight = new_weight
+                self.new_data_available = True
 
     def l_steps_lc(self, msg):
         self.steps_lc(msg,0)
 
     def r_steps_lc(self, msg):
-        self.handle_lc(msg,1)
+        self.steps_lc(msg,1)
 
-    def l_handle_lc(self, msg):
-        self.handle_lc(msg, 'left')
-
-    def r_handle_lc(self, msg):
-        self.handle_lc(msg,'right')
-
-    def handle_lc(self, msg, side):
-        if ('right' in side ):
+    def handle_lc(self, msg):
+        if ('right' in msg.header.frame_id ):
             self.right_handle_msg = msg          
-            self.right_handle_weight = self.fr(msg.force)            
-            # Get handle position in the same frame as the steps.
-            # We need this only once... unless you change steps frame over time?
-            if (self.right_handle_pose==None):
-                self.right_handle_pose= self.get_handle_pose(self.right_step_msg, self.right_handle_msg)
-        elif ('left' in side):
+            self.right_handle_weight = self.fr(msg.force)
+            self.new_data_available = True
+        elif ('left' in msg.header.frame_id):
             self.left_handle_msg = msg
-            self.left_handle_weight  = self.fl(msg.force)             
-            if (self.left_handle_pose==None):
-                self.left_handle_pose= self.get_handle_pose(self.left_step_msg, self.left_handle_msg)
+            self.left_handle_weight  = self.fl(msg.force) 
+            self.new_data_available = True
         else:
             self.get_logger().error("Don't know about which handle are you talking [" + msg.header.frame_id + "]")    
-            return   
-
-        self.leg_load = self.weight - self.left_handle_weight - self.right_handle_weight
+            return                
 
     def steps_lc(self, msg, id):
         if  (id==1):
                 self.right_step_msg = msg
                 self.right_speed = self.right_step_msg.speed
+                self.new_data_available = True
         elif (id==0):
                 self.left_step_msg = msg
                 self.left_speed = self.left_step_msg.speed
+                self.new_data_available = True
         else:
             self.get_logger().error("Don't know about which step are you talking [" + id + "]")    
             return          
@@ -154,74 +142,45 @@ class PartialLoads(Node):
             self.speed_diff = speed_diff_x
 
     def timer_callback(self):
-        if ( (self.left_handle_msg is not None) and  (self.right_handle_msg is not None) and (self.left_step_msg is not None) and  (self.right_step_msg is not None) ):
-                if (self.speed_diff>self.speed_delta):
-                    self.right_leg_load = self.leg_load
-                    self.left_leg_load = 0.0
-                elif (self.speed_diff<-self.speed_delta):
-                    self.right_leg_load = 0.0
-                    self.left_leg_load = self.leg_load
-                else:
-                    # both leg standing. We can't be sure about how much on each one!
-                    self.right_leg_load = -1.0 #0.5 * self.leg_load
-                    self.left_leg_load =  -1.0 #0.5 * self.leg_load
-                   
-                # Build msg and publish
-                # left
-                msg = self.left_step_msg                
-                msg.load = self.left_leg_load
-                self.left_load_pub.publish(msg)
+        if (self.new_data_available):
+            self.new_data_available = False
 
-                #right
-                msg = self.right_step_msg                
-                msg.load = self.right_leg_load
-                self.right_load_pub.publish(msg)
-                #self.get_logger().warn("Weight distribution on legs L(" + str(self.left_leg_load) + ") - R(" + str(self.right_leg_load) + ")")
+            if (not self.first_data_ready ):
+                self.first_data_ready = (self.left_handle_msg is not None) and  (self.right_handle_msg is not None) and (self.left_step_msg is not None) and  (self.right_step_msg is not None)    
 
-                if ((self.right_handle_pose is not None) and (self.right_handle_pose is not None)):
-                    # in case we have double support we dont publish half/half in loads msg, as it would be inaccurate.
-                    # but for centroid calculus we will use it as approximation
-                    if (self.right_leg_load==-1):
+            if ( self.first_data_ready ):
+                    # amount of weight on legs
+                    self.leg_load = self.weight - self.left_handle_weight - self.right_handle_weight
+
+                    # assign weight to supporting leg...
+                    if (self.speed_diff>self.speed_delta):
+                        self.right_leg_load = self.leg_load
+                        self.left_leg_load = 0.0
+                    elif (self.speed_diff<-self.speed_delta):
+                        self.right_leg_load = 0.0
+                        self.left_leg_load = self.leg_load
+                    else:
+                        # Both leg supporting: We can't be sure about how much on each one!
                         self.right_leg_load = 0.5 * self.leg_load
-                        self.left_leg_load  = 0.5 * self.leg_load
-
-                    right_step_position = np.array([self.right_step_msg.position.point.x, self.right_step_msg.position.point.y, 0])
-                    left_step_position = np.array([self.left_step_msg.position.point.x, self.left_step_msg.position.point.y, 0])
-                    right_handle_position = np.array([self.right_handle_pose.position.point.x, self.right_handle_pose.position.point.y, self.right_handle_pose.position.point.z])
-                    left_handle_position = np.array([self.left_handle_pose.position.point.x, self.left_handle_pose.position.point.y, self.left_handle_pose.position.point.z])
-
-                    centroid = ( self.right_leg_load      * right_step_position   + 
-                                 self.left_leg_load       * left_step_position    + 
-                                 self.right_handle_weight * right_handle_position + 
-                                 self.left_handle_weight  * left_handle_position  ) / self.weight
-
-                    centroid_msg = PointStamped()
-                    centroid_msg.header = self.right_step_msg.header
-                    centroid_msg.position.point.x = centroid[0]
-                    centroid_msg.position.point.y = centroid[1]
-                    centroid_msg.position.point.z = centroid[2]
+                        self.left_leg_load =  0.5 * self.leg_load
                     
-                    self.centroid_pub.publish(centroid_msg)
+                    # Build msgs and publish
+                    # left
+                    msg = self.left_step_msg                
+                    msg.load = self.left_leg_load
+                    self.left_load_pub.publish(msg)
 
+                    #right
+                    msg = self.right_step_msg                
+                    msg.load = self.right_leg_load
+                    self.right_load_pub.publish(msg)
+                    #self.get_logger().warn("Weight distribution on legs L(" + str(self.left_leg_load) + ") - R(" + str(self.right_leg_load) + ")")
+            else:                
+                pass
+                #self.get_logger().error("Not all data received yet ...")
         else:
             pass
-            #self.get_logger().error("Not all data received yet ...")                
-
-    # We take 0,0,0 in handles frame and cast it to steps frame.
-    def get_handle_pose(self, step_msg, handle_msg):
-        handle_pos = None
-        if ( (step_msg is not None) and (handle_msg is not None)):
-            handle_orig_rel = PointStamped()
-            handle_orig_rel.header = handle_msg.header
-            handle_orig_rel.point.x = handle_orig_rel.point.y = handle_orig_rel.point.z = 0
-            
-            try:
-                handle_pos = self.buffer.transform(handle_orig_rel, step_msg.position.header.frame_id)
-            except Exception as e:
-                self.get_logger().error("Can't transform handle point into frame [" + step_msg.position.header.frame_id + "]: [" + str(e) + "]")    
-        
-        return handle_pos
-
+                #self.get_logger().error("Not all data received yet ...")
 
 def main(args=None):
     rclpy.init(args=args)
