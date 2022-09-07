53d2476e1ae2950e4caf9805096bf202ee5d4082
Manuel <manolofc@gmail.com>
2022-09-07 10:49:45 +0200

    Added IMU data to web status

--- walker_bringup/config/walker_cfg.bash
+++ walker_bringup/config/walker_cfg.bash
@@ -1,7 +1,8 @@
-# ~/.walker_cfg.bash
-# you should set these accordingly. Dont use '~' for home folder
+# This file should be modified and copied to ~/.walker_cfg.bash
+# It is needed by walker_config.bash script, called by TMuLE (at package walker_bringup) during config. 
+# These two variables should be adjusted accordingly. WARNING: Dont use '~' for home folder
 ROS2_WORKSPACE="/home/ubuntu/workspace/walker_ws"
 NETWORK_INTERFACE="wlan0"
 
-#this should be automatic
+# This should grab IP address on wireless interface. It is needed for the web interface
 ROS2_IP=$(/sbin/ip -o -4 addr list $NETWORK_INTERFACE | grep dynamic | awk '{print $4}' | cut -d/ -f1)
--- walker_bringup/script/walker_config.bash
+++ walker_bringup/script/walker_config.bash
@@ -1,4 +1,5 @@
 # This script assumes you just sourced .walker_cfg.bash 
 source $ROS2_WORKSPACE/install/setup.bash & source $ROS2_WORKSPACE/install/setup.sh
-# find ip on interface wlan0 
-sed -i "235s/.*/    url : \x27ws:\/\/$ROS2_IP:9090\x27/" $ROS2_WORKSPACE/src/WalKit/walker_web_gui/gui/index.html 
+# CoW: This writes wireless ip address on the web interface. 
+# TODO: make web interface to find out its own ip address.
+sed -i "240s/.*/    url : \x27ws:\/\/$ROS2_IP:9090\x27/" $ROS2_WORKSPACE/src/WalKit/walker_web_gui/gui/index.html 
