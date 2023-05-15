bb91f2b1eb5ffc031b51da141688d2f3059d936c
MFernandezCarmona <manolofc@gmail.com>
2023-05-15 10:48:46 +0200

    added dummy user

---
+++ walker_stability/launch/dummy_users.launch.py
@@ -0,0 +1,54 @@
+from os.path import join
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+'''
+ros2 launch stability_grid_map visualize_stability_gridmap.launch.py 
+ros2 launch walker_bringup walker_map_server.launch.py 
+ros2 launch walker_stability dummy_users.launch.py 
+
+'''
+def generate_launch_description():
+    walker_stability_folder = get_package_share_directory('walker_stability')
+
+
+    ld = LaunchDescription()
+    config = join(walker_stability_folder,
+        'config',
+        'users_conf.yaml'
+        )
+        
+    node_1=Node(
+        package = 'walker_stability',
+        executable = 'dummy_user.py',
+        name = 'dummy_user_1',
+        parameters = [config]
+    )
+    ld.add_action(node_1)
+
+    node_2=Node(
+        package = 'walker_stability',
+        executable = 'dummy_user.py',
+        name = 'dummy_user_2',
+        parameters = [config]
+    )
+    ld.add_action(node_2)
+
+    node_3=Node(
+        package = 'walker_stability',
+        executable = 'dummy_user.py',
+        name = 'dummy_user_3',
+        parameters = [config]
+    )
+    ld.add_action(node_3)    
+
+    node_4=Node(
+        package = 'walker_stability',
+        executable = 'dummy_user.py',
+        name = 'dummy_user_4',
+        parameters = [config]
+    )
+    ld.add_action(node_4)
+
+    return ld
--- walker_stability/scripts/dummy_user.py
+++ walker_stability/scripts/dummy_user.py
@@ -21,6 +21,12 @@ from std_msgs.msg import String
 from geometry_msgs.msg import PointStamped, PoseStamped
 from walker_msgs.msg import StabilityStamped
 
+
+'''
+ros2 run walker_stability dummy_user.py --ros-args -p my_str:="Hello world" -p my_int:=5 -p my_double_array:="[4.4, 5.5, 6.6]"
+
+'''
+
 class DummyUser(Node):
 
     def __init__(self):
@@ -30,41 +36,30 @@ class DummyUser(Node):
             namespace='',
             parameters=[
                  ('stability_topic_name', '/user_stability'),
-                 ('user_id', 'user3'),
+                 ('user_id', 'user1'),
+                 ('x_pose', [-1.66, -1.89, -3.22, -2.53,  1.56,  3.83,  2.41,  2.22, -0.77,  0.02, -2.51, -3.38, -1.09 ]),
+                 ('y_pose', [ 3.19,  5.28,  5.89, 15.08, 14.76, 13.59, 10.78,  7.82,  8.32, 14.99, 15.18,  5.87,  3.44 ]),
+                 ('s_pose', [ 1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0 ]),
+                 ('trans_speed',  0.85),
+                 ('timer_period', 0.25),         
                  ('user_tinetti', 0.4)
             ]
         )
 
+        # Get params
         self.stability_topic_name = self.get_parameter('stability_topic_name').value
         self.user_id              = self.get_parameter('user_id').value
         self.user_tinetti         = self.get_parameter('user_tinetti').value
+        self.x_pose               = self.get_parameter('x_pose').value
+        self.y_pose               = self.get_parameter('y_pose').value
+        self.s_pose               = self.get_parameter('s_pose').value
+        self.trans_speed          = self.get_parameter('trans_speed').value
+        self.timer_period         = self.get_parameter('timer_period').value
 
-        # ROS stuff
-        self.stability_pub  = self.create_publisher(StabilityStamped, self.stability_topic_name,  10)
-                
-        self.get_logger().info("dummy user main loop started")  
-
-        # TODO crap here
-        # usuario 1 tin 1 sta 1
-        #x_pose = [-1.66, -1.89, -3.22, -2.53,  1.56,  3.83,  2.41,  2.22, -0.77,  0.02, -2.51, -3.38, -1.09 ]
-        #y_pose = [ 3.19,  5.28,  5.89, 15.08, 14.76, 13.59, 10.78,  7.82,  8.32, 14.99, 15.18,  5.87,  3.44 ]
-        # usuario 2 tin 0.6  sta 0.2
-        #x_pose = [-1.66, -1.89, -3.22, -3.97, -2.53,  1.56,  3.83,  2.41,  2.22, -0.77,  0.02, -2.51, -3.38, -1.09 ]
-        #y_pose = [ 3.19,  5.28,  5.89,  7.32, 15.08, 14.76, 13.59, 10.78,  7.82,  8.32, 14.99, 15.18,  5.87,  3.44 ]
-        # usuario 3 tin 0.4  sta 0.4
-        x_pose = [ 2.01, -1.66, -1.89, -3.22, -3.97, -2.53,  1.56,  3.83,  2.41,  2.22, -0.77,  0.02, -2.51, -3.38, -1.09 ]
-        y_pose = [ 3.06,  3.19,  5.28,  5.89,  7.32, 15.08, 14.76, 13.59, 10.78,  7.82,  8.32, 14.99, 15.18,  5.87,  3.44 ]
-        # usuario 4 tin 0.8  sta 0.6
-        x_pose = [ 2.01, -1.66, -1.89, -3.22, -3.97, -2.53,  1.56,  3.83,  2.41,  2.22, -0.77,  0.02, -2.51, -3.38, -1.09 ]
-        y_pose = [ 3.06,  3.19,  5.28,  5.89,  7.32, 15.08, 14.76, 13.59, 10.78,  7.82,  8.32, 14.99, 15.18,  5.87,  3.44 ]
-        s_pose = [ 0.56,  0.59,  0.58,  0.89,  0.32,  0.58,  0.76,  0.59,  0.78,  0.82,  0.32,  0.99,  0.01,  0.07,  0.24 ]
-
-        self.trans_speed = 0.85
-        timer_period = 0.25
-        
-        self.x_pose = np.array(x_pose)
-        self.y_pose = np.array(y_pose)
-        self.s_pose = np.array(s_pose)
+        # Internal state
+        self.x_pose = np.array(self.x_pose)
+        self.y_pose = np.array(self.y_pose)
+        self.s_pose = np.array(self.s_pose)
 
         x_inc = np.diff(self.x_pose)
         y_inc = np.diff(self.y_pose)
@@ -75,7 +70,7 @@ class DummyUser(Node):
         
         self.t_pose = d/self.trans_speed
 
-        t_pose_upsampled = np.arange(0, self.t_pose[-1], timer_period, dtype=float)
+        t_pose_upsampled = np.arange(0, self.t_pose[-1], self.timer_period, dtype=float)
 
         self.x_pose = np.interp(t_pose_upsampled, self.t_pose, self.x_pose)
         self.y_pose = np.interp(t_pose_upsampled, self.t_pose, self.y_pose)
@@ -83,13 +78,19 @@ class DummyUser(Node):
 
         self.i = 0
 
+        # ROS stuff
+        self.stability_pub  = self.create_publisher(StabilityStamped, self.stability_topic_name,  10)
+                
         # Create a timer 
-        self.timer = self.create_timer(timer_period, self.callback)
+        self.timer = self.create_timer(self.timer_period, self.callback)
+
+        self.get_logger().info("dummy user started")  
+
 
     def callback(self):
         self.sendStability(self.x_pose[self.i], self.y_pose[self.i], self.s_pose[self.i])
 
-        self.get_logger().warn("We are at [" + str(round( self.x_pose[self.i],2)) +  ", " + str(round( self.y_pose[self.i],2)) +  " ].\n\n")   
+        #self.get_logger().warn("We are at [" + str(round( self.x_pose[self.i],2)) +  ", " + str(round( self.y_pose[self.i],2)) +  " ].\n\n")   
 
         if self.i == (np.size(self.y_pose)-1):
             self.destroy_node()
